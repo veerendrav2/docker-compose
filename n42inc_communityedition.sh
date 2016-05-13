48de43d1a07ee347b6359cba998c253fc0a71c73
Basivireddy <basivireddy@users.noreply.github.com>
2016-05-13 15:15:51 +0530

    Update n42inc_communityedition.sh

--- n42inc_communityedition.sh
+++ n42inc_communityedition.sh
@@ -4,3 +4,4 @@ compose_script="n42incdocker_compose.py"
 echo "--- Download agent ---"
 wget -q -O "$compose_script" 'https://raw.githubusercontent.com/N42Inc/docker-compose/master/n42incdocker_compose.py'
 echo $key
+chmod +x "$compose_script"
