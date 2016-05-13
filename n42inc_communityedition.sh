2a4425b83e39612316cc022e5b17fc46f46a24e9
Basivireddy <basivireddy@users.noreply.github.com>
2016-05-13 15:06:59 +0530

    Create n42inc_communityedition.sh

---
+++ n42inc_communityedition.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+key=$1
+compose_script="n42incdocker_compose.py"
+echo "--- Download agent ---"
+wget -q -O "$compose_script" 'https://raw.githubusercontent.com/N42Inc/docker-compose/master/n42incdocker_compose.py'
+echo $key
